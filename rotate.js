77ea0a9bc441c2cd8a1d15c26aaa83c6aad87cd5
shixianqin <1658852581@qq.com>
2018-08-12 20:00:50 +0800

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -4,15 +4,10 @@ function getAngle(a, b) {
 }
 
 export default class Rotate {
-	constructor(options) {
+	constructor(handler) {
 		this.isRotate = false;
 		this.startAngle = 0;
-		this.events = {
-			start: options.start,
-			rotate: options.rotate,
-			end: options.end,
-			cancel: options.cancel
-		}
+		this.handler = handler;
 	}
 
 	start(evt) {
@@ -20,34 +15,25 @@ export default class Rotate {
 		if (touches.length >= 2) {
 			this.isRotate = true;
 			this.startAngle = getAngle(touches[0], touches[1]);
-			if (this.events.start) {
-				this.events.start(evt);
-			}
 		}
 	}
 
 	move(evt) {
-		if (this.isRotate && this.events.rotate) {
+		if (this.isRotate && this.handler) {
 			let touches = evt.touches;
 			evt.angle = this.startAngle - getAngle(touches[0], touches[1]);
-			this.events.rotate(evt);
+			this.handler(evt);
 		}
 	}
 
 	end(evt) {
 		if (this.isRotate) {
 			this.isRotate = false;
-			if (this.events.end) {
-				this.events.end(evt);
-			}
 		}
 	}
 
 	cancel(evt) {
 		if (this.isRotate) {
-			if (this.events.cancel) {
-				this.events.cancel(evt);
-			}
 			this.end(evt);
 		}
 	}
