ffbfedf1463c10675884dbda5ff92f2ef24922f3
shixianqin <1658852581@qq.com>
2018-08-14 12:17:13 +0800

    Update wx-touch.js

--- wx-touch.js
+++ wx-touch.js
@@ -3,6 +3,7 @@
     https://github.com/shixianqin/wx-touch
 */
 
+
 /*
 
 获取一条线相对于垂直的角度
@@ -13,6 +14,7 @@
 function getAngle(a, b) {
     return 180 / Math.PI * Math.atan2(b.clientX - a.clientX, b.clientY - a.clientY);
 }
+
 
 /*
 
@@ -25,48 +27,39 @@ function getDistance(a, b) {
     return Math.sqrt(Math.pow(a.clientX - b.clientX, 2) + Math.pow(a.clientY - b.clientY, 2));
 }
 
-/*
 
-WxTouch
-@param {Object} options
-
-*/
 class WxTouch {
+
+    // @param {Object} options
     constructor(options) {
-
-        // swipe 触发的阈值
-        this.threshold = options.threshold >= 30 ? options.threshold : 30;
-
-        // 把传递的处理器保存到实例中
-        this.events = {
+        this.data = {};
+        this.$threshold = options.threshold >= 30 ? options.threshold : 30;
+        this.$timeout = options.timeout === false ? false : options.timeout >= 300 ? options.timeout : 1000;
+        this.$events = {
             swipe: options.swipe,
             drag: options.drag,
             rotate: options.rotate,
             pinch: options.pinch
-        };
+        }
     }
 
     // @param {TouchEvent} evt
     start(evt) {
-        let touches = evt.touches;
+        let touch = evt.$touch,
+            touches = evt.touches;
 
-        // 标记已经开始触摸
-        this.touched = true;
-
-        // 标记开始触摸的时间
-        this.timeStamp = Date.now();
-
-        // 保存开始触摸的事件对象
-        this.touches = touches;
+        touch.$touched = true;
+        touch.$startTime = Date.now();
+        touch.$touches = touches;
 
         // 当触摸点有两个以上，要保存 rotate 开始的角度，和 pinch 开始的距离
         // 用于 move 的时候计算结果
         if (touches.length > 1) {
-            if (this.events.rotate) {
-                this.startAngle = getAngle(touches[0], touches[1]);
+            if (touch.$events.rotate) {
+                touch.$startAngle = getAngle(touches[0], touches[1]);
             }
-            if (this.events.pinch) {
-                this.startDistance = getDistance(touches[0], touches[1]);
+            if (touch.$events.pinch) {
+                touch.$startDistance = getDistance(touches[0], touches[1]);
             }
         }
     }
@@ -74,21 +67,23 @@ class WxTouch {
     // @param {TouchEvent} evt
     move(evt) {
 
-        // 必须是 start 执行了，才会触发
-        if (this.touched) {
-            let startTouches = this.touches,
+        // evt.$touch 是当前的 WxTouch 事件实例对象
+        let touch = evt.$touch;
+
+        if (touch.$touched) {
+            let startTouches = touch.$touches,
                 moveTouches = evt.touches,
-                events = this.events;
+                events = touch.$events;
 
             // 当有两个以上的触摸点，就认为是 rotate 或者 pinch
             if (moveTouches.length > 1) {
                 if (events.rotate) {
-                    evt.rotateAngle = this.startAngle - getAngle(moveTouches[0], moveTouches[1]);
-                    events.rotate(evt);
+                    evt.rotateAngle = touch.$startAngle - getAngle(moveTouches[0], moveTouches[1]);
+                    events.rotate.call(this, evt);
                 }
                 if (events.pinch) {
-                    evt.scaling = getDistance(moveTouches[0], moveTouches[1]) / this.startDistance;
-                    events.pinch(evt);
+                    evt.scaling = getDistance(moveTouches[0], moveTouches[1]) / touch.$startDistance;
+                    events.pinch.call(this, evt);
                 }
 
                 // 只有一个触摸点的时候，才会触发 drag 事件
@@ -97,37 +92,42 @@ class WxTouch {
                     x: moveTouches[0].clientX - startTouches[0].clientX,
                     y: moveTouches[0].clientY - startTouches[0].clientY
                 };
-                events.drag(evt);
+                events.drag.call(this, evt);
             }
         }
     }
 
     // @param {TouchEvent} evt
     end(evt) {
+        let touch = evt.$touch,
+            touches = touch.$touches;
 
         // 必须是 start 执行了，才会触发
-        if (this.touched) {
-            this.touched = false;
+        // 结束触摸后重置这个标记，以防误触
+        if (touch.$touched) {
+            touch.$touched = false;
 
+            // 当触摸点有两个以上时，不触发 swipe 事件
             // 限制 swipe 滑动时长，当触摸时间过长，就不做处理
-            if (Date.now() - this.timeStamp < 1000) {
+            // 如果传递 timeout === false 就不限制
+            if (touches.length == 1 && (touch.$timeout === false || Date.now() - touch.$startTime < touch.$timeout)) {
                 let endTouch = evt.changedTouches[0],
-                    x = endTouch.clientX - this.touches[0].clientX,
-                    y = endTouch.clientY - this.touches[0].clientY,
+                    x = endTouch.clientX - touches[0].clientX,
+                    y = endTouch.clientY - touches[0].clientY,
                     distanceX = Math.abs(x),
                     distanceY = Math.abs(y);
 
                 // 判断滑动距离达到了阈值，可以触发 swipe 事件
-                if (distanceX >= this.threshold || distanceY >= this.threshold) {
+                if (distanceX >= touch.$threshold || distanceY >= touch.$threshold) {
 
-                    // 标记是否触发了 swipe
+                    // 标记是否触发了滑动事件
                     evt.swiped = true;
 
-                    if (this.events.swipe) {
+                    if (touch.$events.swipe) {
 
-                        // 改变事件的名称，用于回调时候的方向判断
-                        evt.type = distanceX > distanceY ? (x > 0 ? "swiperight" : "swipeleft") : y > 0 ? "swipedown" : "swipeup";
-                        this.events.swipe(evt);
+                        // 回调时候判断方向作相应处理
+                        evt.direction = distanceX > distanceY ? (x > 0 ? "right" : "left") : y > 0 ? "down" : "up";
+                        touch.$events.swipe.call(this, evt);
                     }
                 }
             }
@@ -137,31 +137,35 @@ class WxTouch {
     // 假如可能被中断了触摸，就执行 end 事件
     // @param {TouchEvent} evt
     cancel(evt) {
-        this.end(evt);
+        evt.$touch.end.call(this, evt);
     }
 }
 
 
+
 /*
 
-处理绑定到实例时太过冗余的方法
-@param {Stirng} name
-@param {WxTouch} event
+创建事件
+返回的对象包括 {start[Name], move[Name], end[Name], cancel[Name]}
+使用 es6 扩展运算符直接绑定到实例里面
+
 @param {Object} options
 
 */
-WxTouch.bindEvent = function(name, event, options) {
-    let bind = {};
-    if (!options) {
-        options = {}
-    }
-    ["start", "move", "end", "cancel"].forEach(item => {
-        bind[item + name] = options[item] ? function(evt){
-            options[item].call(this, evt, event);
-        } : event[item].bind(event);
-    });
-    return bind;
-};
+export default function(options) {
+    let touch = new WxTouch(options),
+        mergeEvents = {},
+        events = {},
+        bindTypes = ["start", "move", "end", "cancel"];
 
-//
-export default WxTouch;
+    bindTypes.forEach(type => {
+        let name = type + options.name;
+        mergeEvents[name] = options[type] ? options[type] : touch[type];
+        events[name] = function(evt) {
+            evt.$touch = touch;
+            mergeEvents[name].call(this, evt);
+        }
+    });
+
+    return events;
+}
